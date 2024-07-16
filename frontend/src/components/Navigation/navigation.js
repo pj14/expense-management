eae91882498672f4a3c45bf7f16cc29fa53b9c75
Pratyush <joshi.pratyush14@gmail.com>
2024-07-16 20:47:45 +0100

    navigation fixes complete

--- frontend/src/components/Navigation/navigation.js
+++ frontend/src/components/Navigation/navigation.js
@@ -4,7 +4,7 @@ import avatar from "../../img/avatar.png";
 import { menuItems } from "../../utils/menuItems";
 import { signout } from "../../utils/icons";
 
-function Navigation() {
+function Navigation({ active, setActive }) {
   return (
     <NavStyled>
       <div className="user-con">
@@ -17,7 +17,11 @@ function Navigation() {
       <ul className="menu-items">
         {menuItems.map((item) => {
           return (
-            <li key={item.id}>
+            <li
+              key={item.id}
+              onClick={() => setActive(item.id)}
+              className={active === item.id ? "active" : ""}
+            >
               {item.icon}
               <span>{item.title}</span>
             </li>
@@ -56,7 +60,51 @@ const NavStyled = styled.nav`
       background: #fcf6f9;
       border: 2px solid #ffffff;
       padding: 0.2rem;
-      box-shadow: 0px 1px 17px rgba(0, 0, 0, 0.07);
+      box-shadow: 0px 1px 17px rgba(0, 0, 0, 0.06);
+    }
+    h2 {
+      color: rgba(34, 34, 96, 1);
+    }
+    p {
+      color: rgba(34, 34, 96, 0.6);
+    }
+  }
+  .menu-items {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    li {
+      display: grid;
+      grid-template-columns: 40px auto;
+      align-items: center;
+      margin: 0.6rem 0;
+      font-weight: 500;
+      cursor: pointer;
+      transition: all 0.4s ease-in-out;
+      color: rgba(34, 34, 96, 0.6);
+      padding-left: 1rem;
+      position: relative;
+      i {
+        color: rgba(34, 34, 96, 0.6);
+        font-size: 1.4rem;
+        transition: all 0.4s ease-in-out;
+      }
+    }
+  }
+  .active {
+    color: rgba(34, 34, 96, 1);
+    i {
+      color: rgba(34, 34, 96, 1);
+    }
+    &::before {
+      content: "";
+      position: absolute;
+      left: 0;
+      top: 0;
+      width: 4px;
+      height: 100%;
+      background: #222260;
+      border-radius: 0px 10px 10px 0;
     }
   }
 `;
